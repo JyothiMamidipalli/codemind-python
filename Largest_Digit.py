1ee6239b8a1c058c5dd3d14c9ce095f563170a80
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2023-05-19 15:54:43 +0530

    n=input() print(max(n))

--- Largest_Digit.py
+++ Largest_Digit.py
@@ -1,9 +1,2 @@
-num=int(input())
-large=0
-rem=0
-while (num > 0):
-        rem = num % 10
-        if (rem > large):
-             large = rem
-        num = num / 10;
-print("%d"%large);
+n=input()
+print(max(n))
