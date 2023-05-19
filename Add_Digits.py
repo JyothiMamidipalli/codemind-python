bf7641ec219e6d12d5c46c6686392ef7edc3c463
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2023-05-19 08:45:38 +0530

    n=int(input()) t=n s=0 while(t!=0):     r=t%10     s=s+r     t=t//10     if(t==0 and s>=10):         t=s         s=0 print(s)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,7 +1,11 @@
 n=int(input())
-if(n<=9):
-    print(n)
-elif(n%9==0):
-    print("9")
-else: 
-    print(n%9)
+t=n
+s=0
+while(t!=0):
+    r=t%10
+    s=s+r
+    t=t//10
+    if(t==0 and s>=10):
+        t=s
+        s=0
+print(s)
